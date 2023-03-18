05fa93b091b2577a57ae68414b0ef1f27b465609
harshad1901 <harshadpawar1901@gmail.com>
2023-03-18 15:03:39 +0530

    minor change

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 <head>
 	<title></title>
 	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport"  content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/stayle1.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
